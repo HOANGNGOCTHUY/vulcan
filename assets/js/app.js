dea66d432adfd88756edbf157d2718d03c35face
HOANGNGOCTHUY <hoangngocthuy6@gmail.com>
2022-04-22 14:41:56 +0700

    edit

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,59 +17,58 @@ hambuger.addEventListener("click", function(){
 
 
 //Slider
-    var btns = document.querySelectorAll('.btn');
-    var sliders = document.querySelectorAll('.slider-item');
+    // var btns = document.querySelectorAll('.btn');
+    // var sliders = document.querySelectorAll('.slider-item');
 
-    var currentSlide = 1;
+    // var currentSlide = 1;
 
-    var manualNav = function (manual){
-        sliders.forEach((slide) => {
-            slide.classList.remove('active');
+    // var manualNav = function (manual){
+    //     sliders.forEach((slide) => {
+    //         slide.classList.remove('active');
 
-            btns.forEach((btn) =>{
-                btn.classList.remove('active');
-           });
-        });
+    //         btns.forEach((btn) =>{
+    //             btn.classList.remove('active');
+    //        });
+    //     });
         
-        sliders[manual].classList.add('active');
-        btns[manual].classList.add('active');
-    }
+    //     sliders[manual].classList.add('active');
+    //     btns[manual].classList.add('active');
+    // }
 
-    btns.forEach((btn,i) => {
-        btn.addEventListener("click",() =>{
-             manualNav(i);
-             currentSlide = i;
-        });
-    });
+    // btns.forEach((btn,i) => {
+    //     btn.addEventListener("click",() =>{
+    //          manualNav(i);
+    //          currentSlide = i;
+    //     });
+    // });
 
-    //autoplay navigation
 
-    var repeat = function(activeClass){
-        let active = document.getElementsByClassName('active');
-        let i = 1;
+    // var repeat = function(activeClass){
+    //     let active = document.getElementsByClassName('active');
+    //     let i = 1;
 
-        var repeater = () =>{
-            setTimeout(function(){
-                [...active].forEach((activeSlide) =>{
-                    activeSlide.classList.remove('active');
-                });
-                    sliders[i].classList.add('active');
-                    btns[i].classList.add('active');
-                    i++;
+    //     var repeater = () =>{
+    //         setTimeout(function(){
+    //             [...active].forEach((activeSlide) =>{
+    //                 activeSlide.classList.remove('active');
+    //             });
+    //                 sliders[i].classList.add('active');
+    //                 btns[i].classList.add('active');
+    //                 i++;
 
-                    if(sliders.length == i){
-                        i =0;
-                    }
-                    if(i >= sliders.length){
-                        return ;
-                    }
-                    repeater();
+    //                 if(sliders.length == i){
+    //                     i =0;
+    //                 }
+    //                 if(i >= sliders.length){
+    //                     return ;
+    //                 }
+    //                 repeater();
                 
-            },3000);
-        }
-        repeater();
-    }
-    repeat();
+    //         },3000);
+    //     }
+    //     repeater();
+    // }
+    // repeat();
 
 // -************************************************************
     // 360 độ
