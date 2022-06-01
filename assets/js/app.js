0dd2d456c3634f31eb17900ecd8c7f7893020a85
HOANGNGOCTHUY <hoangngocthuy6@gmail.com>
2022-06-01 15:19:18 +0700

    res

--- assets/js/app.js
+++ assets/js/app.js
@@ -161,58 +161,58 @@ btns.forEach((btn, i) => {
 //   numberOfImages: 50,
 // });
 //comment
-var nagis = document.querySelectorAll(".nagi");
-var experiences = document.querySelectorAll(".experience-items");
+// var nagis = document.querySelectorAll(".experience-btn");
+// var experiences = document.querySelectorAll(".experience-items");
 
-let count = 1;
+// let count = 1;
 
-var manualNav = function (manual) {
-  experiences.forEach((experience) => {
-    experience.classList.remove("change");
+// var manualNav = function (manual) {
+//   experiences.forEach((experience) => {
+//     experience.classList.remove("change");
 
-    nagis.forEach((nagi) => {
-      nagi.classList.remove("change");
-    });
-  });
+//     nagis.forEach((nagi) => {
+//       nagi.classList.remove("change");
+//     });
+//   });
 
-  experiences[manual].classList.add("change");
-  nagis[manual].classList.add("change");
-};
+//   experiences[manual].classList.add("change");
+//   nagis[manual].classList.add("change");
+// };
 
-nagis.forEach((nagi, i) => {
-  nagi.addEventListener("click", () => {
-    manualNav(i);
-    count = i;
-  });
-});
+// nagis.forEach((nagi, i) => {
+//   nagi.addEventListener("click", () => {
+//     manualNav(i);
+//     count = i;
+//   });
+// });
 
 //autoplay navigation
 
-var repeat = function (activeClass) {
-  let active = document.getElementsByClassName("change");
-  let i = 1;
+// var repeat = function (activeClass) {
+//   let active = document.getElementsByClassName("change");
+//   let i = 1;
 
-  var repeater = () => {
-    setTimeout(function () {
-      [...active].forEach((activeSlide) => {
-        activeSlide.classList.remove("change");
-      });
-      experiences[i].classList.add("change");
-      nagis[i].classList.add("change");
-      i++;
+//   var repeater = () => {
+//     setTimeout(function () {
+//       [...active].forEach((activeSlide) => {
+//         activeSlide.classList.remove("change");
+//       });
+//       experiences[i].classList.add("change");
+//       nagis[i].classList.add("change");
+//       i++;
 
-      if (experiences.length == i) {
-        i = 0;
-      }
-      if (i >= experiences.length) {
-        return;
-      }
-      repeater();
-    }, 3000);
-  };
-  repeater();
-};
-repeat();
+//       if (experiences.length == i) {
+//         i = 0;
+//       }
+//       if (i >= experiences.length) {
+//         return;
+//       }
+//       repeater();
+//     }, 3000);
+//   };
+//   repeater();
+// };
+// repeat();
 // *****************************************************************
 // Show hide img
 var check = document.getElementById("checkbox-img");
