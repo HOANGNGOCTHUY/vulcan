57c747a42a66ab232e5e72d9cfdd9fc439227ee0
HOANGNGOCTHUY <hoangngocthuy6@gmail.com>
2022-06-14 17:12:18 +0700

    reponsive 14062022

--- assets/js/app.js
+++ assets/js/app.js
@@ -216,21 +216,3 @@ prevBtn.addEventListener("click", function () {
   curentIndex = (curentIndex - 1 + storys.length) % storys.length;
   storys[curentIndex].classList.add("story-active");
 });
-
-// Video
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    600: {
-      items: 2,
-    },
-    1000: {
-      items: 3,
-    },
-  },
-});
